90ada603d3fbfed21b89142b8da8e78e24cde2b4
Mariia Klymenko <mashaklimenko42@gmail.com>
2017-07-24 16:23:49 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,114 @@
+//const userName = prompt('Please enter yiur name');
+//const helloMessage = 'Hello!' + userName;
+//
+//alert(helloMessage);
+//document.write(helloMessage);
+//console.log(helloMessage);
+
+//let age = Number(prompt ('Enter your age'));
+
+// if(isNan(age)) {
+//     document.write('Please enter corect number');
+// } else {
+//     if (age < 40) {
+//         document.write('You are yong');
+//     } else {
+//         document.write('You are not yong');
+// }
+// }
+
+
+// let mark = Number (prompt('Enter mark'));
+
+// let textMark;
+
+// switch (mark) {
+//     case 1:
+//         textMark = 'Bad';
+//         break;
+//     case 2:
+//         textMark = 'Average';
+//         break;
+//     case 3:
+//             textMark = 'Good';
+//         break;
+//     default:
+//         textMark = 'Incorect mark';
+// }
+// document.write(textMark);
+
+// function add(num1, num2) {
+//     let result = num1 + num2;
+//     return result;
+// }
+
+// let a = 10;
+// let b = 20;
+// let c = add(a, b);
+// document.write(c);
+// document.write(<br>);
+// let d = 40;
+// let f = 50;
+// let e = add(d, f);
+// document.write(e);
+
+// function add3 (num1, num2, num3) {
+//     let result = num1 + num2 + num3;
+//     return result;
+// }
+
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// let d = add3(a, b, c);
+// document.write(d);
+
+// for(var i=9; i >=0; i = i - 1){
+//     if(i % 2 === 0) {
+//     document.write(i);
+//     document.write('<br>');
+// }
+// }
+
+// let age;
+// let correct = false;
+// while (!correct) {
+//   age = Number(prompt('Enter your age'));
+//   correct = !isNan(age);
+//   if (correct) {
+//       alert('Please your correct age');
+//   }
+// }
+
+// document.write('Your age ' + age);
+
+
+// let root  = Math.sqrt(16);
+// document.write(root);
+
+
+// function calcDiscr(a, b, c) {
+//     return a * b - c;
+// }
+
+// function solveQuadratics(a, b, c){
+//     let x1, x2;
+//     let discr;
+//     if (discr < 0) {
+//         return 'No roots';
+//     }
+    
+//     return 'x1= ' + x1 + ', x2=' + x2;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
